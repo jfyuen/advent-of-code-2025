d79173dc662ff872c16af9eb515fd27298f68933
Jean-François YUEN <jfyuen@gmail.com>
2025-12-02 23:18:24 +0100

    days 02 part 1

---
+++ day02/day02.clj
@@ -0,0 +1,43 @@
+(ns day02
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn read-file [filename process-line]
+  (try
+    (with-open [rdr (io/reader filename)]
+      (process-line (line-seq rdr)))
+    (catch java.io.FileNotFoundException e
+      (println "Error: File not found -" filename))))
+
+
+(defn parse-line [line]
+  (let [splitted (str/split line #",")]
+    (map (fn [s] (let [[_ start-str end-str] (re-find #"(\d+)-(\d+)" s)
+                       start (Long/parseLong start-str)
+                       end (Long/parseLong end-str)]
+                   [start end])) splitted)))
+
+
+(defn is-symetric [n]
+  (let [s (str n)
+        len (count s)]
+    (if (even? len)
+      (let [mid (/ len 2)
+            part1 (subs s 0 mid)
+            part2 (subs s mid)]
+        (if (= part1 part2) n 0))
+      0)))
+
+(defn find-symmetric-numbers [a b]
+  (map is-symetric (range a (inc b))))
+
+(defn -main [& args]
+  (let [filename (first args)]
+    (read-file filename
+               (fn [lines]
+                 (let [numbers (mapcat (fn [item]
+                                         (let [[start end] item]
+                                           (find-symmetric-numbers start end)))
+                                       (mapcat parse-line lines))]
+
+                   (println " -> " (reduce + numbers)))))))
